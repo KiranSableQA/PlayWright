a9248602d1f39ed0847679a10a4d15b4906ccd28
Kiran Sable <kiran@example.com>
2026-05-23 16:12:15 +0530

    Commit all current workspace changes including README updates and chapter files

--- Chapter_04_JavaScript_Concepts/19_let_hoisting_block.js
+++ Chapter_04_JavaScript_Concepts/19_let_hoisting_block.js
@@ -1,10 +1,11 @@
 let x = "global";
 
 if (true) {
+
     // TDZ for block-scoped "x" starts here
     // console.log(x);   // ReferenceError (NOT "global"!)
     let x = "block";     // TDZ ends
     console.log(x);      // "block"
 }
 
-console.log(x);
+console.log(x);      // "global" (NOT "block")
